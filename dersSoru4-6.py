86ecd6aea7fcb98e4bd76ca3a6d1f24bebb8bc27
gorkemcetinkaya35 <gorkem.cetinkaya@std.yeditepe.edu.tr>
2025-04-21 17:36:40 +0300

    Add files via upload

---
+++ dersSoru4-6.py
@@ -0,0 +1,17 @@
+#SORU 4 
+
+#x = input("Bir sözcük Yazınız")
+
+#y= len(x)
+
+#z=(x[1:y])
+#print(z)
+
+#SORU 5
+sozcuk=input("Bir sözcük girin")
+sayi= int(input("Bir sayı girin"))
+
+x = sozcuk[:sayi]
+y = sozcuk[sayi:]
+
+print(x+"-"+y)
