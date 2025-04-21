1e578866b0ffe0b41376447e26b017a55389ae2e
gorkemcetinkaya35 <gorkem.cetinkaya@std.yeditepe.edu.tr>
2025-04-21 16:48:19 +0300

    Add files via upload

---
+++ dersSoru1-3.py
@@ -0,0 +1,26 @@
+# Soru 1
+ 
+a= input("Bir sözcük yazınız")
+
+int(len(a))
+
+
+#Soru 2 
+
+x = input("Adınızı Yazınız")
+y = input("Soyadınızı Yazınız")
+
+z =( x [0:1])
+f = (y[0:1])
+print(z+ "."+f)
+
+
+
+#Soru 3
+a= input("Bir Sözcük yazınız")
+
+x=(a[0:1])
+y=len(a)
+
+z=(a[y-1:y]) 
+print(x+z)
